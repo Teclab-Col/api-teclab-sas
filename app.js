e000079d182a011fed56364bdb8656b1126c9b54
Teclab-Col <teclab.developer@teclab.com.co>
2023-11-12 14:22:08 -0500

    feat: ssl certificate

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 // app.js
-const NodeCache = require("node-cache");
-const cache = new NodeCache();
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const usuarioRoutes = require('./routes/usuarioRoutes');
 const authRoutes = require('./routes/authRoutes');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
@@ -15,6 +14,14 @@ app.use(bodyParser.json());
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/auth', authRoutes);
 
-app.listen(port, () => {
-  console.log(`Servidor en ejecución en http://localhost:${port}`);
+// Lee los archivos del certificado y la clave privada
+const privateKey = fs.readFileSync('server.key', 'utf8');
+const certificate = fs.readFileSync('server.cert', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+// Crea un servidor HTTPS
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+  console.log(`Servidor en ejecución en https://localhost:${port}`);
 });
